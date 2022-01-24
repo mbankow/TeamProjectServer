819c1fe52c1bc62d000c544dc543d6a940ef1999
MateuszB <mateusz.bankowski6693@gmail.com>
2022-01-24 10:05:47 +0100

    Add Project dto, exception and repository

--- src/main/java/com/team/project/project/dto/ProjectDTO.java
+++ src/main/java/com/team/project/project/dto/ProjectDTO.java
@@ -1,5 +1,12 @@
 package com.team.project.project.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class ProjectDTO {
     private int id;
     private String name;
---
+++ src/main/java/com/team/project/project/repository/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.team.project.project.repository;
+
+import com.team.project.project.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
