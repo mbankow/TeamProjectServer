db7c87e8abd816776d88249fd29a842927ab9cc1
MateuszB <mateusz.bankowski6693@gmail.com>
2022-01-24 10:04:27 +0100

    Add Student dto, exception and repository

---
+++ src/main/java/com/team/project/student/exception/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.team.project.student.exception;
+
+public class StudentNotFoundException extends RuntimeException{
+    public StudentNotFoundException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/team/project/student/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.team.project.student.repository;
+
+import com.team.project.student.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
