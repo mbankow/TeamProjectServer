a479f74e81920709db43348d0432544ab6060919
MateuszB <mateusz.bankowski6693@gmail.com>
2022-01-24 10:07:02 +0100

    Add Team dto, exception and repository

--- src/main/java/com/team/project/team/dto/TeamDTO.java
+++ src/main/java/com/team/project/team/dto/TeamDTO.java
@@ -1,5 +1,12 @@
 package com.team.project.team.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class TeamDTO {
     private int id;
     private String name;
