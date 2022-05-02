2c241f30025eb792c810a696457cae5dfa076068
MateuszB <mateusz.bankowski6693@gmail.com>
2022-05-02 20:02:11 +0200

    Add person table in database schema

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -25,3 +25,14 @@ create TABLE team (
     id_team INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
 PRIMARY KEY (id_team));
+
+create TABLE person (
+    id_person INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    firm VARCHAR(150) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('ROLE_ADMIN', 'ROLE_MENTOR', 'ROLE_STUDENT') NOT NULL,
+    PRIMARY KEY (id_person));
+)
